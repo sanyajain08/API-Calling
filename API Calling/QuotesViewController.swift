b05d07546596788007bd68d9311440e004f2af9a
Sanya Jain <sanyajain@sanyas-macbook-pro.local>
2018-07-10 14:00:48 -0500

    Set up API Protocols!!!!

--- API Calling/QuotesViewController.swift
+++ API Calling/QuotesViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class QuotesViewController: UITableViewController {
-
+     var quote = [[String: String]]()
+     let apiKey = "1de7c63a2e148d1f9af035a4906d21f4"
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "JOBS"
+        let query = "https://authenticjobs.com/api/?api_key=" + apiKey + "&method=aj.jobs.get&id=1569"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
